ebe5f8e76d334dd3bb2d4c4762ee3cbef0f98635
Leonardo Loaiza <leonardodario.loaiza@unmsm.edu.pe>
2022-08-02 10:42:53 -0500

    boton de pintar, borrador y limpiar

--- assets/js/js.js
+++ assets/js/js.js
@@ -14,3 +14,45 @@ for (let i = 0; i < numAncho; i++) {
 }
 
 // Seleccionar color
+var color = document.querySelector(".color");
+var colorPredeterminado = "#000000";
+
+window.addEventListener("load", startup, false);
+
+function startup() { // evento que se ejecuta cuando se carga la pagina
+    color.addEventListener("change", watchColorPicker);
+    color.value = colorPredeterminado;
+}
+
+var cuadrados = document.querySelectorAll('.cuadrado'); // seleccionar todos los cuadrados
+
+function watchColorPicker(event) { // evento que se ejecuta cuando cambia el color
+    console.log(event.target.value)
+    cuadrados.forEach(cuadrado => {
+        cuadrado.addEventListener('mouseover', cambiarColor)
+    });
+}
+
+function cambiarColor(event) { 
+    event.target.style.backgroundColor = color.value;
+}
+
+// Limpiar cuadrados
+var btnLimpiar = document.querySelector("#limpiarBtn");
+btnLimpiar.addEventListener("click", limpiar);
+
+function limpiar(event) {
+    cuadrados.forEach(cuadrado => {
+        cuadrado.style.backgroundColor = "white";
+    })
+}
+
+// Botón borrador
+var btnBorrador = document.querySelector("#borradorBtn");
+btnBorrador.addEventListener("click", (event) => {
+    color.value = "#ffffff";
+});
+
+// Botón Arcoiris
+var btnArcoiris = document.querySelector("#arcoirisBtn");
+
