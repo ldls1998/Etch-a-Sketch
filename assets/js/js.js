cd2d7ed324311317404e36ca001387ec6d459e79
Leonardo Loaiza <leonardodario.loaiza@unmsm.edu.pe>
2022-08-02 08:54:32 -0500

    cuadrado bien puesto y hecho

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,10 +1,15 @@
 let numAncho = 16;
 
+// Dibujar cuadrados
 var grid = document.querySelector('.grid');
 for (let i = 0; i < numAncho; i++) {
+    fila = document.createElement('div');
+    fila.classList.add('fila');
+    grid.appendChild(fila);
     for (let j = 0; j < numAncho; j++) {
         var cuadrado = document.createElement("div");
         cuadrado.classList.add('cuadrado');
-        grid.appendChild(cuadrado);
-    }    
+        fila.appendChild(cuadrado);
+    }
 }
+
